eaa6c0a5aec741e32c534db47abe04a1d579b2d1
klojtas <w.gasiorowski@gmail.com>
2022-06-21 08:27:36 +0000

    commit from gitpod

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
+print("Starting ..")
+
 @app.route('/')
 def hello_world():
-    return 'Hello, Artifactory test!'
+    return 'Hello, gitpod test 2!'
